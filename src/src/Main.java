3fb652c222cae3b27ad7cae1c822f0130b8f3511
Juan Sanin <juanpablosaninb@gmail.com>
2020-09-02 15:18:37 -0500

    New font added and player name saving created

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,15 +1,22 @@
 package src;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.core.PFont;
 import processing.core.PImage;
 
 public class Main extends PApplet{
-	PImage[] homescreens;
-	PImage[] namescreens;
-	int[][] map;
-	int squareSize;
-	Lebron lbj;
+	private PImage[] homescreens;
+	private PImage[] namescreens;
+	private PFont scoreBoard;
+	private int[][] map;
+	private int squareSize;
+	private Lebron lbj;
 	private int screen;
+	private String name;
+	private ArrayList<Player> players;
+	
 	public static void main(String[] args) {
 		PApplet.main(Main.class.getName());
 
@@ -21,6 +28,7 @@ public class Main extends PApplet{
 
 	public void setup() {
 		ellipseMode(CORNER);
+		textAlign(CENTER);
 		map = new int[][]{ 
 			{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
 			{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
@@ -28,9 +36,9 @@ public class Main extends PApplet{
 			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 			{1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1},
 			{1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1},
-			{1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1},
-			{0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0},
+			{1, 0, 1, 0, 1, 0, 0, 0, 3, 3, 3, 0, 1, 0, 1, 1, 0, 1, 0, 1},
+			{1, 0, 1, 0, 1, 0, 1, 0, 3, 3, 3, 0, 1, 0, 0, 0, 0, 1, 0, 1},
+			{0, 0, 1, 0, 1, 0, 1, 0, 3, 3, 3, 0, 1, 1, 1, 1, 0, 1, 0, 0},
 			{1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
 			{1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1},
 			{1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1},
@@ -50,9 +58,12 @@ public class Main extends PApplet{
 		namescreens[0]=loadImage("images/NameScreen.png");
 		namescreens[1]=loadImage("images/NameScreenGo.png");
 		namescreens[2]=loadImage("images/NameScreenError.png");
-
+		scoreBoard= createFont("fonts/60s Scoreboard.ttf", 32);
+		
+		name="";
+		players= new ArrayList<Player>();
 		lbj= new Lebron(this, 40, 120);
-		screen=3;
+		screen=2;
 	}
 
 	public void draw() {
@@ -82,35 +93,25 @@ public class Main extends PApplet{
 			if (mouseX > 275 && mouseX < 525 && mouseY > 350 && mouseY < 410) {
 				image(namescreens[1],0,0,800,600);
 			}
+			fill(0);
+			textFont(scoreBoard);
+			text(name, 400, 306);
+			break;
+		case 3:
+			image(namescreens[2],0,0,800,600);
 			break;
 
-		case 3:
-			for (int columns = 0; columns < 20; columns++) {
-				for (int rows = 0; rows < 15; rows++) {
-					if(map[rows][columns] == 0) {
-						noFill();
-						stroke(255);
-						rect(0+ (columns * squareSize), 0 + (rows * squareSize), squareSize, squareSize);	
-					}
-					if(map[rows][columns] == 1) {
-						fill(0);
-						stroke(255);
-						rect(0+ (columns * squareSize), 0 + (rows * squareSize), squareSize, squareSize);	
-					}
-					if(map[rows][columns] == 2) {
-						fill(255,0,0);
-						stroke(255);
-						rect(0+ (columns * squareSize), 0 + (rows * squareSize), squareSize, squareSize);	
-					}  
-				}
+		case 4:
+				paintMatrix();
 				lbj.draw();
-
-			}
+				System.out.println(players.get(0).getName());
+			
 
 			break;
 
 		}
 		fill(0,255,0);
+		textSize(12);
 		text("" + mouseX + "," + mouseY, mouseX, mouseY);
 	}
 
@@ -131,8 +132,16 @@ public class Main extends PApplet{
 			}
 			break;
 		case 2:
-			if (mouseX > 275 && mouseX < 525 && mouseY > 350 && mouseY < 410) {
+			if (mouseX > 275 && mouseX < 525 && mouseY > 350 && mouseY < 410 && name.equals("")) {
 				screen=3;
+			}else if (mouseX > 275 && mouseX < 525 && mouseY > 350 && mouseY < 410) {
+				players.add(new Player(name));
+				screen=4;
+			}
+			break;
+		case 3:
+			if (mouseX > 562 && mouseX < 592 && mouseY > 185 && mouseY < 215) {
+				screen=2;
 			}
 			break;
 
@@ -142,47 +151,99 @@ public class Main extends PApplet{
 	public void keyPressed() {
 
 		switch(screen) {
-		case 3:
-			if (key == CODED) {
-				if (keyCode == UP) {
-					if (lbj.getMatY() - 1 > -1 && map[lbj.getMatY() - 1][lbj.getMatX()] == 0) {
-						lbj.move(1);
-					}
-				}
-			}
-			if (key == CODED) {
-				if (keyCode == DOWN) {
-					if (lbj.getMatY() + 1 < 15 && map[lbj.getMatY() + 1][lbj.getMatX()] == 0) {
-						lbj.move(2);
-					}
-				}
-			}
-			if (key == CODED) {
-				if (keyCode == LEFT) {
-					if(lbj.getMatX() ==0 &&  lbj.getMatY()==8) {
-						lbj.teleportLeft();
-					}else {
-						if (lbj.getMatX() - 1 > -1 && map[lbj.getMatY()][lbj.getMatX()-1] == 0) {
-							lbj.move(3);
-						}
-
-					}
-				}
-			}
-			if (key == CODED) {
-				if (keyCode == RIGHT) {
-					if(lbj.getMatX() ==19 &&  lbj.getMatY()==8) {
-						lbj.teleportRight();
-					}else {
-					if (lbj.getMatX() + 1 < 20 && map[lbj.getMatY()][lbj.getMatX()+1] == 0) {
-						lbj.move(4);
-					}
-					}
-				}
-			}
+		case 2:
+			typeName();
+			break;
+		case 4:
+			moveUp();
+			moveDown();
+			moveLeft();
+			moveRight();
 
 		}
 	}
 
+	public void paintMatrix() {
+		for (int columns = 0; columns < 20; columns++) {
+			for (int rows = 0; rows < 15; rows++) {
+				if(map[rows][columns] == 0) {
+					noFill();
+					stroke(255);
+					rect(0+ (columns * squareSize), 0 + (rows * squareSize), squareSize, squareSize);	
+				}
+				if(map[rows][columns] == 1) {
+					fill(0);
+					stroke(255);
+					rect(0+ (columns * squareSize), 0 + (rows * squareSize), squareSize, squareSize);	
+				}
+				if(map[rows][columns] == 2) {
+					fill(255,0,0);
+					stroke(255);
+					rect(0+ (columns * squareSize), 0 + (rows * squareSize), squareSize, squareSize);	
+				}  
+				if(map[rows][columns] == 3) {
+					fill(0,255,0);
+					stroke(255);
+					rect(0+ (columns * squareSize), 0 + (rows * squareSize), squareSize, squareSize);	
+				}  
+			}
+	}
+	}
+	public void typeName() {
+		if(key==BACKSPACE) {
+			if (name != null && name.length() > 0 ) {
+		        name = name.substring(0, name.length() - 1);
+		    }
+		}else if(key!=CODED){
+			if(name.length()<=15) {
+				name+=key;
+			}
+			
+		}
+	}
+	public void moveUp() {
+		if (key == CODED) {
+			if (keyCode == UP) {
+				if (lbj.getMatY() - 1 > -1 && map[lbj.getMatY() - 1][lbj.getMatX()] == 0) {
+					lbj.move(1);
+				}
+			}
+		}
+	}
+	public void moveDown() {
+		if (key == CODED) {
+			if (keyCode == DOWN) {
+				if (lbj.getMatY() + 1 < 15 && map[lbj.getMatY() + 1][lbj.getMatX()] == 0) {
+					lbj.move(2);
+				}
+			}
+		}
+	}
+	public void moveLeft() {
+		if (key == CODED) {
+		if (keyCode == LEFT) {
+			if(lbj.getMatX() ==0 &&  lbj.getMatY()==8) {
+				lbj.teleportLeft();
+			}else {
+				if (lbj.getMatX() - 1 > -1 && map[lbj.getMatY()][lbj.getMatX()-1] == 0) {
+					lbj.move(3);
+				}
 
+			}
+		}
+	}}
+	public void moveRight() {
+		if (key == CODED) {
+			if (keyCode == RIGHT) {
+				if(lbj.getMatX() ==19 &&  lbj.getMatY()==8) {
+					lbj.teleportRight();
+				}else {
+				if (lbj.getMatX() + 1 < 20 && map[lbj.getMatY()][lbj.getMatX()+1] == 0) {
+					lbj.move(4);
+				}
+				}
+			}
+		}
+		
+	}
 }
--- src/src/Player.java
+++ src/src/Player.java
@@ -7,9 +7,43 @@ public class Player {
 	
 	public Player(String name) {
 		this.name=name;
+		score=0;
+		time="0:00";
 		
 	}
 	
 	public void calculateTime() {}
+	public void increaseScore(int points) {
+		score+=points;
+		
+	}
+	public void decreaseScore(int points) {
+		score-=points;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
 
 }
